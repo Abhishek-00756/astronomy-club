7b0e67db3631511a04b5f67910d15954d0ed48c7
Abhishek-00756 <birabhi012@gmail.com>
2026-09-24 16:37:54 +0530

    Allow missing member photos

--- data/members/joint-secretaries/members.ts
+++ data/members/joint-secretaries/members.ts
@@ -1,7 +1,7 @@
 export type Member = {
   id: string;
   photoId: string;
-  photo: string;
+  photo?: string;
   name: string;
   bio: string;
   domain?: string;
@@ -12,7 +12,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-01",
     photoId: "JS-01",
-    photo: "/members/joint-secretaries/JS-01.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -20,7 +19,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-02",
     photoId: "JS-02",
-    photo: "/members/joint-secretaries/JS-02.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -28,7 +26,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-03",
     photoId: "JS-03",
-    photo: "/members/joint-secretaries/JS-03.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -36,7 +33,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-04",
     photoId: "JS-04",
-    photo: "/members/joint-secretaries/JS-04.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -44,7 +40,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-05",
     photoId: "JS-05",
-    photo: "/members/joint-secretaries/JS-05.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -52,7 +47,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-06",
     photoId: "JS-06",
-    photo: "/members/joint-secretaries/JS-06.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -60,7 +54,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-07",
     photoId: "JS-07",
-    photo: "/members/joint-secretaries/JS-07.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -68,7 +61,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-08",
     photoId: "JS-08",
-    photo: "/members/joint-secretaries/JS-08.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -76,7 +68,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-09",
     photoId: "JS-09",
-    photo: "/members/joint-secretaries/JS-09.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
@@ -84,7 +75,6 @@ export const jointSecretaries: Member[] = [
   {
     id: "JS-10",
     photoId: "JS-10",
-    photo: "/members/joint-secretaries/JS-10.jpg",
     name: "Name to be added",
     bio: "",
     domain: "",
