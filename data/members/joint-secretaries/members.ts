96ad2b47f18bb12f4deda084bf262e7e38bc84eb
Abhishek-00756 <birabhi012@gmail.com>
2026-09-24 16:04:58 +0530

    Add joint secretary member data

---
+++ data/members/joint-secretaries/members.ts
@@ -0,0 +1,94 @@
+"use client";
+
+export type Member = {
+  id: string;
+  photoId: string;
+  photo: string;
+  name: string;
+  bio: string;
+  domain?: string;
+  regNo?: string;
+};
+
+export const jointSecretaries: Member[] = [
+  {
+    id: "JS-01",
+    photoId: "JS-01",
+    photo: "/members/joint-secretaries/JS-01.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-02",
+    photoId: "JS-02",
+    photo: "/members/joint-secretaries/JS-02.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-03",
+    photoId: "JS-03",
+    photo: "/members/joint-secretaries/JS-03.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-04",
+    photoId: "JS-04",
+    photo: "/members/joint-secretaries/JS-04.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-05",
+    photoId: "JS-05",
+    photo: "/members/joint-secretaries/JS-05.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-06",
+    photoId: "JS-06",
+    photo: "/members/joint-secretaries/JS-06.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-07",
+    photoId: "JS-07",
+    photo: "/members/joint-secretaries/JS-07.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-08",
+    photoId: "JS-08",
+    photo: "/members/joint-secretaries/JS-08.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-09",
+    photoId: "JS-09",
+    photo: "/members/joint-secretaries/JS-09.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+  {
+    id: "JS-10",
+    photoId: "JS-10",
+    photo: "/members/joint-secretaries/JS-10.jpg",
+    name: "Name to be added",
+    bio: "",
+    domain: "",
+  },
+];
