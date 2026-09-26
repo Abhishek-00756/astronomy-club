b8d9e21c00ee7151326e94b28282da36ec74cec5
Abhishek-00756 <birabhi012@gmail.com>
2026-09-26 17:21:44 +0530

    Further optimize hero star rendering

--- components/three/SpaceScene.tsx
+++ components/three/SpaceScene.tsx
@@ -11,7 +11,7 @@ function MovingStars() {
   const pointsRef = useRef<THREE.Points>(null);
 
   const { positions, baseZ, speeds } = useMemo(() => {
-    const count = 1050;
+    const count = 850;
     const positions = new Float32Array(count * 3);
     const baseZ = new Float32Array(count);
     const speeds = new Float32Array(count);
@@ -126,7 +126,7 @@ function SpaceObjects({
       <Stars
         radius={110}
         depth={70}
-        count={1500}
+        count={1250}
         factor={1.8}
         saturation={0}
         fade
